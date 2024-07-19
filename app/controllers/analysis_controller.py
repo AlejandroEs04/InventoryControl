457d9a68d090103bd03ab4d243c76db4de543301
Alexanderel04 <2004.estrada.lopez@gmail.com>
2024-07-19 09:28:27 -0600

    'requeriments txt edited'

--- app/controllers/analysis_controller.py
+++ app/controllers/analysis_controller.py
@@ -1,9 +1,11 @@
 from flask import Blueprint, request, jsonify
+from flasgger import swag_from
 from app.services.data_service import analyze_data
 
 analysis_bp = Blueprint('analysis', __name__)
 
 @analysis_bp.route('/', methods=['POST'])
+@swag_from('swagger_docs/analyze.yml')
 def analyze():
     data = request.get_json()
     result = analyze_data(data)
--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,10 @@
 from flask import Flask
+from flasgger import Swagger
 from app.controllers.analysis_controller import analysis_bp
 
 def create_app():
     app = Flask(__name__)
+    Swagger(app)
     
     # Here will be every routes
     app.register_blueprint(analysis_bp, url_prefix='/analysis')
